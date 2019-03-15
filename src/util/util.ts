778587437d8a3ba31040df8fb425722275545cb9
wzs <zhensheng.five@gmail.com>
2019-03-15 19:56:23 +0800

    去掉微信生成器分支

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,4 @@
-const isMobile = function() {
+export const isMobile = function() {
   let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
   return flag;
 }
-
-export isMobile;
