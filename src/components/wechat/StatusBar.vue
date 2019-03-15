cb96226f872b475ee5945372faef3ae367386597
wzs <zhensheng.five@gmail.com>
2019-03-15 19:43:50 +0800

    css绘制close

--- src/components/wechat/StatusBar.vue
+++ src/components/wechat/StatusBar.vue
@@ -20,9 +20,39 @@ export default class StatusBar extends Vue {
 
 <style scoped lang="scss">
 @import "../../assets/css/iphonex.scss";
-.status{
+.status {
   height: $wechat-status-height;
   padding: 10px 4px;
-  
+
+  .back {
+    width: 2rem;
+    height: 2rem;
+    font-size: 2rem;
+    border: 0px;
+    color: black;
+    background: transparent;
+  }
+
+  .back::before {
+    position: absolute;
+    top: 0;
+    content: "";
+    height: 1.5rem;
+    width: 0.15rem;
+    background: black;
+    transform: rotate(45deg);
+    border-radius: 1rem;
+  }
+
+  .back::after {
+    position: absolute;
+    top: 0;
+    content: "";
+    height: 1.5rem;
+    width: 0.15rem;
+    background: black;
+    transform: rotate(-45deg);
+    border-radius: 1rem;
+  }
 }
 </style>
